0d7908242284f7513fbbda2296feada1d5670de7
Kousuek Takeuchi <k.takeuchi@warrantee.co.jp>
2018-05-18 17:46:55 +0900

    既にダウンロード済みの場合は、再ダウンロードしない設定を追加

--- datatools/histrical.py
+++ datatools/histrical.py
@@ -10,9 +10,15 @@ DOMAIN = 'http://www.histdata.com'
 BASE_URL = DOMAIN + '/download-free-forex-data/?/metatrader/1-minute-bar-quotes/{}'
 
 
-def download(url, form, path=None):
+def download(url, form, path=None, ignore_exists=True):
     if path is None:
         path = '.data/{}/{}.zip'.format(form['fxpair'], form['datemonth'])
+    extract_path = path.split('.zip')[0]
+    print(extract_path)
+
+    if ignore_exists and os.path.exists(path):
+        return
+
     # フォルダ作成
     dir_path = '/'.join(path.split('/')[:-1])
     if not os.path.exists(dir_path):
@@ -33,8 +39,6 @@ def download(url, form, path=None):
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
     }
     res = requests.post(url, data=form, headers=headers, stream=True)
-    extract_path = path.split('.zip')[0]
-    print(extract_path)
     if res.status_code == 200:
         with open(path, 'wb') as f:
             for chunk in res.iter_content(chunk_size=1024):
@@ -54,18 +58,18 @@ def get_pair_monthly_link(pair):
 
 def get_download_form(url):
     res = requests.get(url)
-    soup = BeautifulSoup(res.content)
+    soup = BeautifulSoup(res.content, 'html.parser')
     hiddens = soup.select('form#file_down input[type="hidden"]')
     return dict([[hidden["name"], hidden["value"]] for hidden in hiddens])
 
-def download_local(path='.data', pair=None):
+def download_local(path='.data', pair=None, ignore_exists=True):
     # 全てのペアリストに対してダウンロード先のリンクを取得
     pair_list = [pair] if pair else PAIR_LIST
     for pair in PAIR_LIST:
         urls = get_pair_monthly_link(pair)
         for url in urls:
             form = get_download_form(url)
-            download(url, form)
+            download(url, form, ignore_exists=ignore_exists)
 
 
 def load_local(path='.data'):
